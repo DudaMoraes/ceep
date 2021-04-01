fb2d2dff98a11f333da765b1372d5cdd46ef9c55
Duda Moraes <duda200103@gmail.com>
2021-03-31 21:51:55 -0300

    adicionado função de delete

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import "./estilo.css";
+import deleteSVG from "../../assets/img/delete.svg"
 class CardNota extends Component {
+  apagar(){
+    const index = this.props.index
+    this.props.apagarNota(index)
+  }
+
+
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <img src={deleteSVG} alt="lixeira" onClick={this.apagar.bind(this)}/>
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
     );
   }
+
 }
 
 export default CardNota;
